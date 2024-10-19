09b531833009727372c90f21b66137dbe392891f
Hamelsie <93395332+Hamelsie@users.noreply.github.com>
2024-10-19 22:17:54 +0700

    Create intro.js

---
+++ intro.js
@@ -0,0 +1,8 @@
+// ------audio------------
+var buttonSong = document.querySelector('.button')
+var mySong = document.getElementById("song")
+buttonSong.onclick= function(){
+    if(mySong.paused){
+        mySong.play()
+    }
+}
